94267a4c0bbd893f6ec9885e1049af7e42a272c2
ookuboc5399 <ankle0614@gmail.com>
2023-07-24 14:30:25 +0900

    useref確認

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const handleOnChange = (e) => setName(e.target.value)
+  const inputEl = useRef(null);
+  const handleOnClick = () => inputEl.current.focus();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={inputEl} type='text' value={name} onChange={handleOnChange} />
+      <p>名前：{name}</p>
+      <button onClick={handleOnClick}>inputにフォーカス</button>
     </div>
   );
 }
